7978e917154325023675c22bbd2c0f0c070d6b94
mrbalky <mrbalky@gmail.com>
2014-05-06 18:52:29 -0600

    progress on season adjust

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 irrigation-caddy
 ================
 
-simple scripts for automating irrigation caddy
+Simple scripts for automating IrrigationCaddy
+
+The IrrigationCaddy web UI is excellent, but I did miss the "season adjust" feature of my old controller.
+
+I also have an inadequate compressor for blowing out the lines in the fall, so wanted to automate the multiple cycles
+required to blow out each zone.
+
+These scripts require BASH and wget.
--- seasonadjust.sh
+++ seasonadjust.sh
@@ -1,19 +1,66 @@
 #!/bin/bash
+#-----------------------------------------------------------------------
+# Set an IrrigationCaddy program, and optionally adjust zone run times
+# proportionally based on a percentage of the run times defined in the
+# program.
+#-----------------------------------------------------------------------
 
-# base and default values
-prog=1
-ndays=2
-zoneMins=(20 20 20 10 10 19 12 10 0)
-startTimes=(21:00 0:00 0:00 0:00 0:00)
-allowRun=yes
+function usage {
+    echo "
+USAGE:
+    `basename $0` [-a <pct>] [-p <zone program>] <IrrigationCaddy host>"
+    exit 1
+}
 
+# Options
+while getopts "a:p:" flag
+do
+   case "$flag" in
+      a) adjustPct=$OPTARG
+         ;;
+      p) programSettings=$OPTARG
+         ;;
+      *) usage
+         ;;
+   esac
+done
+shift $((OPTIND-1))
+programSettings=${programSettings:-`dirname $0`/program1_settings}
+adjustPct=${adjustPct:-100}
+
+# IrrigationCaddy host name or ip address is required
 host="$1"
-pct=${2:-100}
 
-data="doProgram=1&allowRun=$allowRun&everyNDays=$ndays&pgmNum=$prog"
+# Check!
+if [ $# -lt 1 ]; then
+    usage
+fi
 
-for n in {0..4}; do
-    t=${startTimes[$n]}
+# Include the prgramming settings
+if [ ! -e $programSettings ]; then
+    echo Program settings file $programSettings not found
+    exit 1
+fi
+. $programSettings
+
+# every n days and even/odd handling
+if [ "$ndays" != "" ] && [ $ndays -gt 0 ]; then
+    days="&everyNDays=$ndays"
+elif [ "$evenOdd" != "" ]; then
+    days="&evenodd=$evenOdd"
+fi
+
+# Start building post data
+data="doProgram=1&allowRun=$allowRun&pgmNum=$prog$days"
+
+# Run on these days
+for day in ${runDays[@]}; do
+    data="$data&day_$day=1"
+done
+
+# Up to 5 start times each day
+let n=0
+for t in "${startTimes[@]}"; do
     hr=${t%:*}
     mn=${t#*:}
     merid=am
@@ -26,18 +73,21 @@ for n in {0..4}; do
     fi
     stime="&stHr$n=$hr&startTime$n=$hr:$mn+$merid&stMin$n=$mn&merid$n=$merid"
     #echo $stime
-    data="$data$stime"
+    data="$data$stime&stStat$n=1"
     #echo $data
+    let n=$n+1
 done
 
+# Finaaly, the zone run times, with percentage adjust
 for n in {0..8}; do
     t=${zoneMins[$n]}
-    t=$(($t*$pct/100))
+    t=$(($t*$adjustPct/100))
     n=$(($n+1))
     dur="&z${n}durHr=0&z${n}durMin=$t"
     #echo $dur
     data="$data$dur"
 done
 
+# Go babe!
 echo wget -S -T 30 -t 1 -O - --post-data "$data" "http://$host/program.htm"
 wget -S -T 30 -t 1 -O - --post-data "$data" "http://$host/program.htm"
