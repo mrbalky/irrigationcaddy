bd3380a526bce770b076ba280eb62fc417ed37ef
mrbalky <mrbalky@gmail.com>
2014-05-24 13:56:11 -0600

    runnow web page now proxies commands to caddy

--- runnow.php
+++ runnow.php
@@ -3,27 +3,26 @@
 $irrigationCaddy = "192.168.1.60";
 
 function minSelect($zone) {
-?>
-<select name="<?="z${zone}durMin"?>">
-  <option value="1">1</option>
-  <option value="2">2</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="5">5</option>
-  <option value="6">6</option>
-  <option value="7">7</option>
-  <option value="8">8</option>
-  <option value="9">9</option>
-  <option value="10">10</option>
-</select>
-<?
+  ?>
+  <select name="<?="z${zone}durMin"?>">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+  </select>
+  <?
 }
 
 function runNowForm($zone) {
     global $irrigationCaddy;
 ?>
-    <!-- <form method="POST" action="http://requestb.in/18anybe1">  -->
-    <form method="POST" action="http://<?=$irrigationCaddy?>/program.htm">
+    <form method="POST" action="<?=$_SERVER['PHP_SELF']?>">
         <b>Zone <?=$zone?>:</b>
         <?
         minSelect($zone);
@@ -45,12 +44,34 @@ function runNowForm($zone) {
         <input type="hidden" name="runNow" value="1"/>
         <input type="hidden" name="pgmNum" value="4"/>
         <input type="submit" value="Run"/>
-    </form>
-<? } ?>
+    </form> <?
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+  if ( isset($_POST['stop']) )
+    $url = "http://$irrigationCaddy/stopSprinklers.htm";
+  else
+    $url = "http://$irrigationCaddy/program.htm";
+
+  $options = array(
+      'http' => array(
+          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method'  => 'POST',
+          'content' => http_build_query($_POST),
+      ),
+  );
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  echo "<pre>";
+  print_r($result);
+  echo "</pre>";
+}
+?>
 <head>
 </head>
 <body>
-    <form method="POST" action="http://<?=$irrigationCaddy?>/stopSprinklers.htm">
+    <form method="POST" action="<?=$_SERVER['PHP_SELF']?>">
         <input type="submit" value="Stop Active Zone"/>
         <input type="hidden" name="stop" value="active"/>
     </form>
@@ -65,6 +86,7 @@ runNowForm(7);
 runNowForm(8);
 runNowForm(9);
 ?>
+
 </body>
 </html>
 
